5e6babeedb08a35c227c9679cd06056a0c0bdf97
MoonriverD <tochifai@icloud.com>
2020-02-07 15:06:41 +0800

    no message

---
+++ Script/WorldTime.js
@@ -0,0 +1,200 @@
+// 作者：congcong 已添加emoji.
+// Surge4: cron "0 7-23 * * *" script-path=https://raw.githubusercontent.com/Delecomp/Selfiles/master1/Script/WorldTime.js
+// QuantumultX: 0 7-23 * * * WorldTime.js
+// #region 固定头部
+let isQuantumultX = $task != undefined; //判断当前运行环境是否是qx
+let isSurge = $httpClient != undefined; //判断当前运行环境是否是surge
+// 判断request还是respons
+// down方法重写
+var $done = (obj={}) => {
+    var isRequest = typeof $request != "undefined";
+    if (isQuantumultX) {
+        return isRequest ? $done({}) : ""
+    }
+    if (isSurge) {
+        return isRequest ? $done({}) : $done()
+    }
+}
+// http请求
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+// cookie读写
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+// 消息通知
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+// #endregion 固定头部
+
+// #region 网络请求专用转换
+if (isQuantumultX) {
+    var errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            url.method = 'POST';
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            //为了兼容qx中fetch的写法,所以永不reject
+            return new Promise((resolve, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        if (response) {
+                            response.body = data;
+                            resolve(response, {
+                                error: error
+                            });
+                        } else {
+                            resolve(null, {
+                                error: error
+                            })
+                        }
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        if (response) {
+                            response.body = data;
+                            resolve(response, {
+                                error: error
+                            });
+                        } else {
+                            resolve(null, {
+                                error: error
+                            })
+                        }
+                    })
+                }
+            })
+
+        }
+    }
+}
+// #endregion 网络请求专用转换
+
+// #region cookie操作
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+// #endregion
+
+// #region 消息通知
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+// #endregion
+Date.prototype.format = function(fmt) {
+	var date = {
+	        "M+": this.getMonth() + 1,
+	        "d+": this.getDate(),
+	        "h+": this.getHours(),
+	        "m+": this.getMinutes(),
+	        "s+": this.getSeconds(),
+	        "q+": Math.floor((this.getMonth() + 3) / 3),
+	        "S": this.getMilliseconds()
+	    };
+    if (/(y+)/i.test(fmt)) {
+    	fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in date) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+        	fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+        }
+    }
+    return fmt;
+};
+
+const areas = [
+               ["🎡UTC", +0],["🇺🇸纽约", -5],["🇯🇵东京", +9],["🇫🇷巴黎", +2],["🇦🇺悉尼", +10]
+           ]
+
+time();
+
+async function time() {
+	var now = new Date(); 
+	var content = "";
+	var week = "🇨🇳北京 "+calcTime(+8)+" UTC+8 星期" + "日一二三四五六".charAt(now.getDay());
+	content += week;
+	for (var i in areas) {
+		var u = valPlus(areas[i][1]);
+        content += "\n"+areas[i][0]+" "+calcTime(u)+" UTC" + u;
+    }
+	
+    $notification.post('今日时间', "", content);
+    $done();
+}
+
+function calcTime(offset) { 
+    var d = new Date(); 
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000); 
+    var nd = new Date(utc + (3600000 * offset)); 
+    return nd.format("yyyy-MM-dd hh:mm:ss"); 
+}
+
+function valPlus(offset) { 
+    if(offset == 0)
+    	return "";
+    else if (offset > 0)
+    	return "+" + offset;
+    else return offset;
+}
