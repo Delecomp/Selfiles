e651bca26351578080e978e5d38302d31323053c
MoonriverD <tochifai@icloud.com>
2020-02-09 20:59:45 +0800

    no message

---
+++ Script/Sleepzy.js
@@ -0,0 +1,34 @@
+/*
+[rewrite_local]
+^https:\/\/subs\.platforms\.team\/apple\/verifyTransaction$ url script-response-body sleepzy.js
+hostname = subs.platforms.team
+*/
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj = {
+ "error": 0,
+ "data": {
+  "is_valid": true,
+  "may_expire": false,
+  "is_offer_eligible": true,
+  "in_app": {
+   "com.apalonapps.smartalarmfree.01y_07dt_SUB00004": {
+    "is_valid": true,
+    "cancelled": false,
+    "may_expire": true,
+    "is_trial": true,
+    "is_intro": false,
+    "transaction_id": "260000622851642",
+    "purchase_date_ms": "1572445394000",
+    "expired": false,
+    "expires_date_ms": "2014297417000",
+    "remaining_time_ms": "1898956800000"
+   }
+  },
+  "consumable_inapp": [],
+  "hash": "2f119a8992f0aca546927c607221afcc"
+ }
+};
+body = JSON.stringify(obj);
+$done(body);
