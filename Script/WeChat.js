c166731169228f55d3b4ad3a440cebdbba69c71b
MoonriverD <tochifai@icloud.com>
2020-03-11 11:50:52 +0800

    Added

--- Script/WeChat.js
+++ Script/WeChat.js
@@ -1,7 +1,15 @@
 /*
-[Script]
-http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://Choler.github.io/Surge/Script/WeChat.js
+QuantumultX:
 
+[rewrite_local]
+^https://mp\.weixin\.qq\.com/mp/getappmsgad url request-body WeChat.js
+[mitm]
+hostname = mp.weixin.qq.com
+
+Surge:
+
+[Script]
+http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://raw.githubusercontent.com/Delecomp/Selfiles/Profiles/Script/WeChat.js
 [MITM]
 hostname = mp.weixin.qq.com
 */
