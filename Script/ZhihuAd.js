18b6c07041a476723bc12609a211bd8fead7bad5
MoonriverD <tochifai@icloud.com>
2020-03-24 20:49:34 +0800

    Update

--- Script/ZhihuAd.js
+++ Script/ZhihuAd.js
@@ -1,15 +1,16 @@
 /* Collected by Delecomp
 QuantumultX:
-^https:\/\/api\.zhihu\.com\/appview\/api\/v4\/answers\/.*\/recommendations url reject-img
-^https?:\/\/(api|www)\.zhihu\.com\/(moments(\/recommend)?\?(action|feed_type)|topstory\/recommend|.*\/questions|market\/header|people|appview\/(v2|p)\/(answer\/)?\d{1,10}\?no\_image\=false(\&article\_fixed\_bottom\=1)?\&X\-SUGER\=) url script-response-body ZhihuAd.js
+^https:\/\/pic\d\.zhimg\.com\/70\/ url reject-img
+https://(api|www).zhihu.com/(ab|adx|fringe|commercial|ad-style-service|.*(recommendations|extended|featured-comment-ad)|api/v4/mcn/) url reject
+^https?:\/\/api\.zhihu\.com\/(moments\?(action|feed_type)|topstory\/recommend|.*\/questions|market\/header|people) url script-response-body ZhihuAd.js
 [MITM]
 hostname = api.zhihu.com, www.zhihu.com （加上自己屏蔽的ip后，用正则表达式屏蔽)
 
 (因QuantumultX最新版移除empty_sni_enabled参数，改为在mitm中直接匹配相关ip，所以请自行使用抓包工具屏蔽广告ip！)
 
 Surge:
-URL-REGEX,^https:\/\/pic\d\.zhimg\.com\/70\/
-URL-REGEX,https://(api|www).zhihu.com/(ab|adx|fringe|commercial|ad-style-service|.*(recommendations|extended|featured-comment-ad)|api/v4/mcn/)
+URL-REGEX,^https:\/\/pic\d\.zhimg\.com\/70\/, reject
+URL-REGEX,https://(api|www).zhihu.com/(ab|adx|fringe|commercial|ad-style-service|.*(recommendations|extended|featured-comment-ad)|api/v4/mcn/), reject
 AND,((USER-AGENT,osee2*), (OR,((IP-CIDR,0.0.0.0/0,no-resolve),(IP-CIDR6,::/0,no-resolve))))
 [Scripts]
 http-response ^https?:\/\/(api|www)\.zhihu\.com\/(moments(\/recommend)?\?(action|feed_type)|topstory\/recommend|.*\/questions|market\/header|people|appview\/(v2|p)\/(answer\/)?\d{1,10}\?no\_image\=false(\&article\_fixed\_bottom\=1)?\&X\-SUGER\=) requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/primovist/ScriptsForSurge/master/Scripts/Zhihu.js
